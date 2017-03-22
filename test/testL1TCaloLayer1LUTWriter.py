e72add287aec4d46df41e7b1e2ae6a2927ac57e6
Nick Smith <nick.smith@cern.ch>
2017-03-22 21:26:47 +0100

    Comment LUT writer config, default behavior is 2016

--- test/testL1TCaloLayer1LUTWriter.py
+++ test/testL1TCaloLayer1LUTWriter.py
@@ -12,17 +12,9 @@ options.parseArguments()
 
 # import of standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
-process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
 process.load('FWCore.MessageService.MessageLogger_cfi')
-process.load('Configuration.EventContent.EventContent_cff')
-process.load('Configuration.Geometry.GeometryExtended2016Reco_cff')
-process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
-process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
-process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 from Configuration.AlCa.GlobalTag import GlobalTag
-#process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_data', '')
-process.GlobalTag = GlobalTag(process.GlobalTag, '80X_dataRun2_HLT_v9', '')
 
 process.source = cms.Source('EmptySource',
     firstRun = cms.untracked.uint32(options.runNumber)
@@ -33,17 +25,41 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
 
 process.load('L1Trigger.L1TCaloLayer1Spy.l1tCaloLayer1LUTWriter_cfi')
 process.l1tCaloLayer1LUTWriter.fileName = options.outputFile
-
 process.p = cms.Path(process.l1tCaloLayer1LUTWriter)
-
 process.schedule = cms.Schedule(process.p)
 
-# HF 1x1 TPs need special ES source as of 2016/03/21
-from L1Trigger.Configuration.customiseReEmul import L1TEventSetupForHF1x1TPs
-process = L1TEventSetupForHF1x1TPs(process)
+#
+# Things that actually matter start here
+#
+
+# Will affect the HCAL LUTs, and CaloParams if they are ever in GT...
+process.GlobalTag = GlobalTag(process.GlobalTag, '90X_dataRun2_Express_v2', '')
+
 
 # To get L1 CaloParams, until in GT
-process.load('L1Trigger.L1TCalorimeter.caloStage2Params_cfi')
+process.load('L1Trigger.L1TCalorimeter.caloStage2Params_2016_v3_3_1_cfi')
 
-# To get CaloTPGTranscoder
+
+# HCAL Plan1 geometry can be loaded form RecoDB if using a recent enough run number
+# Beware that the HCAL compression LUTs ARE RUN DEPENDENT
+# 2016 running used LUTs created with the 'Extended2016' geometry
+#process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
+process.load('Configuration.Geometry.GeometryExtended2016Reco_cff')
+# These geometry files might be useful if the GT doesn't have plan 1 in any IOV
+#process.load("Configuration.Geometry.GeometryExtended2017Plan1_cff")
+#process.load("Configuration.Geometry.GeometryExtended2017Plan1Reco_cff")
+
+
+# HF 1x1 TPs need special ES source as of 2016/03/21
+# This was used for 2016 LUTs but appears unnecessary as the 1x1 has been
+# incorporated in the geometry if an appropriate IOV is given (i.e. run number after 1x1 installed)
+from L1Trigger.Configuration.customiseReEmul import L1TEventSetupForHF1x1TPs
+# process = L1TEventSetupForHF1x1TPs(process)
+
+
+# To get the CaloTPGTranscoder, which decodes the HCAL compression LUT
 process.load('SimCalorimetry.HcalTrigPrimProducers.hcaltpdigi_cff')
+# From an email by Aleko:
+# "For simulation you want LUTGenerationMode=True; (it's about which conditions to use for input LUTs)."
+# Not sure what that means...
+#process.HcalTPGCoderULUT.LUTGenerationMode = cms.bool(False)
